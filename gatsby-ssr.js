f46a36fba3f7577a61e53f20f712f732ce947c9e
KarenCagape <karenjoycagape@gmail.com>
2022-11-11 14:55:56 +0800

    added meta pixel

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -13,6 +13,24 @@ const PreBodyComponents = [
     </div>,
 ];
 
-exports.onRenderBody = ({ setPreBodyComponents }) => {
+const HeadComponents = [
+    <script
+        dangerouslySetInnerHTML={{
+            __html: ` !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1164804407479110');
+            fbq('track', 'PageView');`,
+        }}
+    />,
+];
+
+exports.onRenderBody = ({ setPreBodyComponents, setHeadComponents }) => {
+    setHeadComponents(HeadComponents);
     setPreBodyComponents(PreBodyComponents);
 };
