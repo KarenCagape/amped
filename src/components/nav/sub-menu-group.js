2bd174e9638928bb121f65426eccd779ac17eb5a
KarenCagape <karenjoycagape@gmail.com>
2023-09-06 18:47:46 +0800

    updated submenu with 5 columns

--- src/components/nav/sub-menu-group.js
+++ src/components/nav/sub-menu-group.js
@@ -5,7 +5,7 @@ export function SubMenuGroup({ image, links, ...rest }) {
     return (
         <div tw="xl:grid xl:grid-cols-7 justify-between gap-4 2xl:gap-6 lg:px-4" {...rest}>
             {image ? <div tw="xl:col-span-2 pointer-events-none max-w-[33.333%] xl:max-w-full mx-auto">{image}</div> : ""}
-            {links ? <div tw="xl:col-span-5 flex-none flex flex-col pt-4">{links}</div> : ""}
+            {links ? <div tw="xl:col-span-5 flex-none flex flex-col">{links}</div> : ""}
         </div>
     );
 }
